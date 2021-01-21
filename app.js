1fb3c2bd570abb27b0138412cf47fab3eeec0c06
hujasperson <77764895+hujasperson@users.noreply.github.com>
2021-01-20 21:28:02 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+// CODE EXPLAINED channel
 
+// Select the Elements 
+const clear = document.querySelector(".clear");
+const dataElement = document.getElementById(".date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+//Classes names
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_THROUGH = "lineThrough";
+const SORT_BY_PEN = "pending";
+
+// Show todays date
+const options = {weekday : "long", month:"short", day:"numeric"}
+const today = new Date();
+
+dataElement.innerHTML = today.toLocaleDateSTring("en-US", options);
